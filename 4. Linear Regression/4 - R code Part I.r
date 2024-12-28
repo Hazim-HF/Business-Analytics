f57d29f34dca472319f920982cb5f66c9c18f454
Hazim-HF <hazim.fitri.hf@gmail.com>
2024-12-28 18:38:21 +0800

    Update folder

---
+++ 4. Linear Regression/4 - R code Part I.r
@@ -0,0 +1,51 @@
+data = read.csv("C:\\Users\\PC 11\\Desktop\\P152419\\BUSINESS ANALYTICS\\Ch4_marketing.csv")
+model <- lm(revenues ~ marketing_total, data = data)
+str(model)
+model$fitted.values
+res_manual <- data$revenues - model$fitted.values
+res_model <- model$residuals
+yhat_model <- model$fitted.values
+yhat_manual <- model$coefficients[1] + (data$marketing_total * model$coefficients[2])
+
+df <- data.frame(cbind(yhat_model, yhat_manual, res_model, res_manual))
+print(df)
+str(df)
+?abline()
+
+# Normality assumption
+par(mfrow = c(1,2))
+hist(model$residuals)
+qqnorm(model$residuals)
+qqline(model$residuals)
+
+# Equal Variance assumption
+plot(model$fitted.values, model$residuals)
+abline(0,0, lwd=3);abline(h = c(-6.5,6.5), lwd=3,lty=3)
+
+# Predict
+newdata <- data.frame(marketing_total = 460)
+predict.lm(model,newdata, level = 0.99, interval = 'predict')
+?predict.lm()
+
+
+library(dplyr)
+set.seed(4510)
+
+market_sample <- sample_frac(data,0.3,repalce = TRUE)
+sample_model <- lm(revenues ~ marketing_total, data = market_sample)
+sample_model
+confint(sample_model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
